590e28e14473a052c9658cb824db6abd379322ee
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-03-03 14:58:53 +0530

    First Commit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,50 +1,69 @@
 import React, {useState} from 'react'
 
-
-export default function TextForm(props) {
-    const [text, setText] = useState('');
-    // Updating text variable :
-    // text = "New text"; //Wrong way
-    // setText("New Text"); //Right way
+export default function TextForm(props){
     const handleUpClick = () => {
+        let text = document.getElementById('myBox').value;
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to uppercase","success");
     }
     const handleLoClick = () => {
-        let newText = text.toLowerCase();
-        setText(newText);
+      let text = document.getElementById('myBox').value;
+      let newText = text.toLowerCase();
+      setText(newText);
+      props.showAlert("Converted to lowercase","success");
     }
     const handleClearClick = () => {
-        setText("");
+      if(document.getElementById('myBox').value.length>0){
+        let newText = "";
+        setText(newText);
+        props.showAlert("Text cleared","success");
+      }else{
+        props.showAlert("Text is already empty","danger");
+      }
+    }
+    const handleCopy = () => {
+      let text = document.getElementById('myBox');
+      if(text.value.length<1){
+        props.showAlert("Text is empty","danger");
+      }else{
+      text.select();
+      navigator.clipboard.writeText(text.value);
+      props.showAlert("Text copied to clipboard","success");
+      }
+    }
+    const handleExtraSpaces = () => {
+      let text = document.getElementById('myBox').value;
+      let newText = text.split(/[ ]+/);
+      setText(newText.join(" "));
+      props.showAlert("Extra spaces removed","success");
     }
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
-    
+
+    const [text, setText] = useState('Enter text here');
+
   return (
     <>
-        <div className="container">
-            <h2>{props.heading}</h2>
-            <div className="mb-4">
-                <label htmlFor="MyBox" className="form-label">{props.subHeading}</label>
-                <textarea className="form-control" id="MyBox" rows="8" value={text} onChange={handleOnChange}></textarea>
-            </div>
-            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
-            <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+      <div className='container' style={{color: props.mode==='light'?'black':'white'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} style={{color: props.mode==='light'?'black':'white',backgroundColor: props.mode==='light'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <div className="container my-4">
-            <h3>Text Analysis</h3>
-            <p>Number of word : {text.split(" ").length}</p>
-            <p>Number of Characters : {text.length}</p>
-            <p>Time to read {0.008 * text.split(" ").length}</p>
-        </div>
-        <div className="container my-2">
-            <h3>Preview</h3>
-            <p>{text}</p>
-        </div>
-        
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1 my -1" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+      </div>
+      <div className="container my-2" style={{color: props.mode==='light'?'black':'white'}}>
+        <h2>Your Text Summary</h2>
+        <p>{text.length<1?0:text.trim().split(" ").length} words and {text.length} characters</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter Text in the box above to preview it here"}</p>
+      </div>
     </>
+    
   )
 }
-
