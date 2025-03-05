9c40c9565ac861fa405b65921e712c6f73d2115f
Nishant Wagahde <nishant.waghade17@gmail.com>
2025-03-05 18:01:38 +0530

    Deployment hotfixes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, {useState} from 'react'
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
 
